b159f69a2f587061187f995a91a08a4f06a0010f
Yuekai Jia <equation618@gmail.com>
2025-05-13 01:46:04 +0800

    Upgrade rust-toolchain to 1.89.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 #![cfg_attr(not(test), no_std)]
 #![cfg_attr(doc, feature(doc_auto_cfg))]
-#![feature(naked_functions)]
 #![doc = include_str!("../README.md")]
 
 #[macro_use]
--- src/x86_64/context.rs
+++ src/x86_64/context.rs
@@ -261,28 +261,26 @@ impl TaskContext {
     }
 }
 
-#[naked]
+#[unsafe(naked)]
 unsafe extern "C" fn context_switch(_current_stack: &mut u64, _next_stack: &u64) {
-    unsafe {
-        naked_asm!(
-            "
-            .code64
-            push    rbp
-            push    rbx
-            push    r12
-            push    r13
-            push    r14
-            push    r15
-            mov     [rdi], rsp
+    naked_asm!(
+        "
+        .code64
+        push    rbp
+        push    rbx
+        push    r12
+        push    r13
+        push    r14
+        push    r15
+        mov     [rdi], rsp
 
-            mov     rsp, [rsi]
-            pop     r15
-            pop     r14
-            pop     r13
-            pop     r12
-            pop     rbx
-            pop     rbp
-            ret",
-        )
-    }
+        mov     rsp, [rsi]
+        pop     r15
+        pop     r14
+        pop     r13
+        pop     r12
+        pop     rbx
+        pop     rbp
+        ret",
+    )
 }
